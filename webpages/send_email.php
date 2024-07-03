646ae5a3b54181add43930519e8017811749dd62
Oguz Gur <oguz.gur.cs@gmail.com>
2024-07-03 17:46:36 +0100

    v1.0.0

--- webpages/send_email.php
+++ webpages/send_email.php
@@ -1,35 +1,36 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $message = trim($_POST["message"]);
+    $question = trim($_POST["question"]);
 
-    if (empty($name) || empty($email) || empty($message)) {
+    if (empty($name) || empty($email) || empty($question)) {
         http_response_code(400);
         echo "Please fill out all fields.";
         exit;
     }
 
-    $to = "oguz.gur.cs@example.com"; 
-
-    $subject = "New Contact Form Submission from $name";
-
+    $to = "oguz.gur.cs@gmail.com"; 
+    $subject = "New Question from $name";
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
+    $email_content .= "Question:\n$question\n";
 
-    $headers = "From: $name <$email>";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
 
     if (mail($to, $subject, $email_content, $headers)) {
         http_response_code(200);
-        echo "Thank you! Your message has been sent.";
     } else {
         http_response_code(500);
-        echo "Something went wrong. Please try again later.";
     }
 } else {
     http_response_code(403);
-    echo "Access forbidden.";
 }
+require("index.html");
 ?>
